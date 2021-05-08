6292b8711eed8bfec20622b5098665098612e84e
unknown <zark28>
2021-05-08 10:33:03 +0000

    media queries update

--- src/components/products/Productnav.jsx
+++ src/components/products/Productnav.jsx
@@ -8,7 +8,7 @@ const Productnav = ()=>{
         <ul className='productnav'>
             
             <NavLink exact to='/'activeClassName='activeNav'><Productnavitem navItem='Design' navColor='var(--navColor1)'/> </NavLink>
-            <NavLink to='/list2' activeClassName='activeNav'><Productnavitem navItem='Developments' navColor='var(--navColor2)'/></NavLink>
+            <NavLink to='/list2' activeClassName='activeNav'><Productnavitem navItem='Development' navColor='var(--navColor2)'/></NavLink>
             <NavLink to='/list3' activeClassName='activeNav'><Productnavitem navItem='Marketing' navColor='var(--navColor3)'/></NavLink>
             <NavLink to='/list4' activeClassName='activeNav'><Productnavitem navItem='IT & Software' navColor='var(--navColor4)'/></NavLink>
             <NavLink to='/list5' activeClassName='activeNav'><Productnavitem navItem='Business' navColor='var(--navColor5)'/></NavLink>
